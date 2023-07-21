1f556085092dd06a798b151052959c9874e2a7ed
Felipe Chaves <84556078+felipe-chaves15@users.noreply.github.com>
2023-07-21 15:15:49 -0600

    Finalización de semana 10

---
+++ src/main/java/com/tienda/service/impl/UsuarioDetailsServiceImpl.java
@@ -0,0 +1,51 @@
+package com.tienda.service.impl;
+
+import com.tienda.dao.UsuarioDao;
+import com.tienda.domain.Rol;
+import com.tienda.domain.Usuario;
+import com.tienda.service.UsuarioDetailsService;
+import jakarta.servlet.http.HttpSession;
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service("userDetailsService")
+public class UsuarioDetailsServiceImpl implements UsuarioDetailsService, UserDetailsService {
+
+    @Autowired
+    private UsuarioDao usuarioDao;;
+
+    @Autowired
+    private HttpSession session;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // Buascar en BD el usuario
+        Usuario usuario = usuario = usuarioDao.findbyUsername(username);
+
+        if (usuario == null) {
+            throw new UsernameNotFoundException("El usuario" + username + " no existe");
+        }
+
+        //varibales de sesion
+        session.removeAttribute("usuarioImagen");
+        session.setAttribute("usuarioImagen", usuario.getRutaImagen());
+
+        //Extraer los roles
+        var roles = new ArrayList<GrantedAuthority>();
+        for (Rol rol : usuario.getRoles()) {
+            roles.add(new SimpleGrantedAuthority(rol.getNombre()));
+        }
+
+        return new User(usuario.getUsername(), usuario.getPassword(), roles);
+    }
+
+}
